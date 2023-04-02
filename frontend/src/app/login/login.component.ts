efa65f928ed3f7b00ad9f1e89ce9536e14751993
NisreenMiri <65628636+NisreenMiri@users.noreply.github.com>
2023-04-02 13:03:05 +0200

    Update login.component.ts

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
 }
+
