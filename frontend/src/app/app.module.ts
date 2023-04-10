9195daa664f1942a32bd66f03baa35afd4cb1120
nisreen miri <nsreenmiri@gmail.com>
2023-04-10 20:20:28 +0200

    //Authentication

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,8 +8,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { BlogComponent } from './components/blog/blog.component';
 import { LoginComponent } from './components/login/login.component';
-import { ApiVisualiserComponent } from './components/api-visualiser/api-visualiser.component';
-import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,13 +17,12 @@ import { HttpClientModule } from '@angular/common/http';
     FooterComponent,
     HomeComponent,
     BlogComponent,
-    LoginComponent,
-    ApiVisualiserComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash";
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  hideShowPass() {
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
+    this.isText ? this.type = "text" : this.type = "password";
+  }
+
+  onSubmit(){
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value)
+    }else
+    {
+      alert("form is vaild")
+      this.validateAllFormFilds(this.loginForm);
+    }
+
+  }
+
+  private validateAllFormFilds(formGroup:FormGroup){
+    Object.keys(formGroup.controls).forEach(field=>{
+      const control = formGroup.get(field);
+      if(control instanceof FormControl){
+        control.markAsDirty({onlySelf:true});
+      }else if (control instanceof FormGroup) {
+        this.validateAllFormFilds(control)
+      }
+    })
+  }
 
 }
