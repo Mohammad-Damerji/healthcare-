2770b5aade378c7351210aff0caf5205c3cdd716
brandyyyyy <37420472+brandyyyyy@users.noreply.github.com>
2023-04-12 12:14:35 +0200

    Add files via upload

---
+++ models/Xray/chest_xray_prediction.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from os import listdir
+from os.path import join, isfile, isdir
+from glob import glob
+
+from keras.preprocessing.image import ImageDataGenerator, smart_resize
+from keras.applications.densenet import DenseNet121
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras.models import Model, load_model
+from keras import backend as K
+
+from PIL import Image
+from tqdm import tqdm
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+from keras.models import load_model
+from keras.preprocessing.image import ImageDataGenerator
+
+import random
+
+import cv2
+import numpy as np
+from keras import backend as K
+from keras.preprocessing import image
+from tensorflow.keras import optimizers, losses, metrics
+from tensorflow.compat.v1.logging import INFO, set_verbosity
+
+
+# In[7]:
+
+
+labels = ['Cardiomegaly', 
+          'Emphysema', 
+          'Effusion', 
+          'Hernia', 
+          'Infiltration', 
+          'Mass', 
+          'Nodule', 
+          'Atelectasis',
+          'Pneumothorax',
+          'Pleural_Thickening', 
+          'Pneumonia', 
+          'Fibrosis', 
+          'Edema', 
+          'Consolidation']
+
+
+# In[9]:
+
+
+model = load_model('xray_model.h5')
+
+
+# In[10]:
+
+
+def diag_list(predicted_values):
+    diagnosis = []
+    
+    return sorted(zip(predicted_values, labels), reverse=True)[:3]
+
+default_folder = "xray_image"
+#accepts a folder which needs to be in this format *chestxray_input_folder\images\(image goes here)*
+def disease_probability(chestxray_input_folder="xray_image", model=model):
+    image_generator = ImageDataGenerator(
+    samplewise_center=True,
+    samplewise_std_normalization= True)
+    test_datagen = image_generator.flow_from_directory(directory=chestxray_input_folder,
+                                                    class_mode=None,
+                                                    batch_size=1,
+                                                    shuffle=False,
+                                                    target_size=(320,320))
+    pred = (model.predict(test_datagen, verbose = 1))
+    return diag_list(pred[0])
+
