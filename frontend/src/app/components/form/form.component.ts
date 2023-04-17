5527a63240e02e408e20adcf139322f819e4bba3
nisreen miri <nsreenmiri@gmail.com>
2023-04-17 10:05:00 +0200

    //form validation

--- frontend/src/app/components/form/form.component.ts
+++ frontend/src/app/components/form/form.component.ts
@@ -1,34 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-  disease: FormGroup
+  formForm!: FormGroup;
 
-  constructor () {
-    this.disease = new FormGroup({
-      gender: new FormControl(),
-      age: new FormControl()
-    })
-  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    
+    this.formForm = this.fb.group({
+      Age: ['', Validators.required],
+      bmi: ['', Validators.required],
+      avg_glucose_level: ['', Validators.required],
+      gender: ['', Validators.required],
+      residence_type : ['', Validators.required]
+    });
+  }
+ 
+  get gender() {
+    return this.formForm.get('gender');
   }
 
-  onSubmit(form: FormGroup) {
-    console.log(form.value.gender);
-    console.log(form.value.age);
-    
+  get residence_type (){
+    return this.formForm.get('residence_type');
+
   }
 
-  /*
-  gender,
-  age
-  */
+  onSubmit(){
+    if(this.formForm.valid){
+      console.log(this.formForm.value)
+    }else
+    {
+      alert("form is vaild")
+      this.validateAllFormFilds(this.formForm);
+    }
+
+  }
+
+  private validateAllFormFilds(formGroup:FormGroup){
+    Object.keys(formGroup.controls).forEach(field=>{
+      const control = formGroup.get(field);
+      if(control instanceof FormControl){
+        control.markAsDirty({onlySelf:true});
+      }else if (control instanceof FormGroup) {
+        this.validateAllFormFilds(control)
+      }
+    })
+  }
 
 }
