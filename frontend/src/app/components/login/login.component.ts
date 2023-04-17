bc95b5205109d47f57e5453bc0d3f13144107ae8
Robert Fiko <fiko.robert@gmail.com>
2023-04-17 08:20:16 +0200

    user navigatiton on login

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -25,6 +25,9 @@ export class LoginComponent implements OnInit {
       username: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
+    if (this.userSrv.isLoggedIn()) {
+      this.router.navigate(['welcome'])
+    }
   }
 
   hideShowPass() {
