5da5e56b662343a76b7a78474e022ec5fb5ce22d
shadi-danhash <shdashd1998@gmail.com>
2023-04-18 23:28:06 +0200

    admin register

---
+++ backend/app/diagnosis/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Disease, Tag
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('id', 'name')
+
+
+class DiseaseSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True)
+
+    class Meta:
+        model = Disease
+        fields = ('id', 'name', 'description', 'tags', 'images')
