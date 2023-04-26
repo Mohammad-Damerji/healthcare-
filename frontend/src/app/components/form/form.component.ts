ca2cf92941b0bdf255f43ac870c1f415db05efb8
Robert Fiko <fiko.robert@gmail.com>
2023-04-26 19:02:23 +0200

    stroke detection

--- frontend/src/app/components/form/form.component.ts
+++ frontend/src/app/components/form/form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { RestAPIService } from 'src/app/services/rest-api.service';
 import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-form',
@@ -9,21 +10,28 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class FormComponent implements OnInit {
   formForm!: FormGroup;
+  success: string;
+  error: string;
 
 
-  constructor(private fb: FormBuilder, private userSrv: UserService, private router: Router) { 
+  constructor(private fb: FormBuilder, private userSrv: UserService, private router: Router, private api: RestAPIService) { 
     if (!userSrv.isLoggedIn()) {
       this.router.navigate([''])
     }
+    this.success = ""
+    this.error = ""
   }
 
   ngOnInit(): void {
     this.formForm = this.fb.group({
-      Age: ['', Validators.required],
+      age: ['', Validators.required],
       bmi: ['', Validators.required],
       avg_glucose_level: ['', Validators.required],
       gender: ['', Validators.required],
-      residence_type : ['', Validators.required]
+      Residence_type : ['', Validators.required],
+      hypertension:  ['', Validators.required],
+      heart_disease: ['', Validators.required],
+      smoking_status: ['default', Validators.required],
     });
   }
  
@@ -39,9 +47,25 @@ export class FormComponent implements OnInit {
   onSubmit(){
     if(this.formForm.valid){
       console.log(this.formForm.value)
-    }else
-    {
-      alert("form is vaild")
+      this.api.predictStroke(this.formForm.value).subscribe(e => {
+
+        //after the stroke prediction run another prediction
+        console.log(e);
+
+        if (e.success) {
+          this.error = ""
+          this.success = e.message
+        }
+        else {
+          this.error = e.message
+          this.success = ""
+        }
+
+      })
+    }
+    
+    else {
+      alert("form is invaild")
       this.validateAllFormFilds(this.formForm);
     }
 
